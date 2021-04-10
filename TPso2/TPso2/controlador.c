d9b2956ea40f0818833e03bf44bf021af9a69c3f
DrDranky <paul.ionut.bob@gmail.com>
2021-04-10 12:50:46 +0100

    lançar apenas uma instancia do controlador done

--- TPso2/TPso2/controlador.c
+++ TPso2/TPso2/controlador.c
@@ -5,10 +5,19 @@
 #include <stdio.h>
 
 int _tmain(int argc, TCHAR* argv[]) {
+	//previne poder ter mais do que uma instância do mesmo programa a correr em simultâneo.
+	CreateMutexA(0, FALSE, "Local\\$myprogram$"); // try to create a named mutex
+	if (GetLastError() == ERROR_ALREADY_EXISTS) {   // did the mutex already exist?
+		puts("Already running!");
+		return -1;								  // quit; mutex is released automatically
+	}
+
 #ifdef UNICODE
 	(void)_setmode(_fileno(stdin), _O_WTEXT);
 	(void)_setmode(_fileno(stdout), _O_WTEXT);
 	(void)_setmode(_fileno(stderr), _O_WTEXT);
 #endif
 	_tprintf(L"Olá mundo!\n");
+	Sleep(5000); //testar linha 9 a 13
+	return 0;
 }
