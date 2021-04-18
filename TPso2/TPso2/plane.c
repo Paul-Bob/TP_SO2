d07154f1fd9740709398f5f4e5281745f1b0fd2a
Liliana Faustino <lilianafaustino92@gmail.com>
2021-04-18 15:33:11 +0100

    Esqueleto feito, venham dai as threads

--- TPso2/TPso2/plane.c
+++ TPso2/TPso2/plane.c
@@ -6,7 +6,9 @@
 
 #include "plane.h"
 
-int getArguments(int argc, TCHAR* argv[], pPlane plane) {
+#define SIZE 200
+
+int getArguments(int argc, LPTSTR argv[], pPlane plane) {
 
 	//TODO: Falta o aeroporto que não faço ideia como vou saber qual o aeroporto onde estou ?????' -.-''' "#$"#$ fuck my life
 
@@ -16,11 +18,11 @@ int getArguments(int argc, TCHAR* argv[], pPlane plane) {
 	}
 	
 
-	if (sscanf_s(argv[1], "%d", &plane->maxCapacity) == 0) {
+	if (_stscanf_s(argv[1], L"%d", &plane->maxCapacity) == 0) {
 		_ftprintf(stderr, L"Capacidade do avião inválida\n");
 		return -1;
 	}
-	if (sscanf_s(argv[2], "%d", &plane->velocity) == 0) {
+	if (_stscanf_s(argv[2], L"%d", &plane->velocity) == 0) {
 		_ftprintf(stderr, L"Velocidade do avião inválida\n");
 		return -1;
 	}
@@ -28,7 +30,70 @@ int getArguments(int argc, TCHAR* argv[], pPlane plane) {
 	return 1;
 }
 
-int _tmain(int argc, TCHAR* argv[]) {
+int move(int curX, int curY, int finalX, int finalY, int* nextX, int* nextY) {
+	return 1;
+}
+
+int verifyNewPosition(int next_x, int next_y) {
+	return 1; //TODO: Meter isto a avaliar se a posicao está livre
+}
+
+void notifyController() {
+	_tprintf(L"[DEBUG] Amigo controlador, cheguei, tá? \n"); //TODO: Avisar o controlador que cheguei ou atualizar a minha posicao
+}
+
+void initTrip(pPlane plane) {
+	
+	while (1) {
+		Sleep(1000 / (DWORD)plane->velocity);
+
+		int nextX = 0, nextY = 0;
+		int result = move(plane->current.x, plane->current.y, plane->final.x, plane->final.y, &nextX, &nextY);
+
+		if (result == 2) {
+			continue;
+		}
+		else if (result == 0) {
+			_tprintf(L"[DEBUG] Cheguei ao meu destino, tenho que avisar o controlador \n"); 
+			notifyController();
+			return;
+		}
+		else {
+			if (verifyNewPosition(nextX, nextY) == 1) {
+				plane->current.x = nextX;
+				plane->current.y = nextY;
+				
+				notifyController();
+				
+			}
+			else {
+				continue; //TODO: Podemos melhorar esta estrategia, para já estamos só a aguardar que os gordos desocupem a loja
+			}
+		}
+	}
+
+	return;
+}
+
+int processCommand(TCHAR* command, pPlane plane) {
+	if (!_tcscmp(command, TEXT("exit"))) {
+		return 0;
+	}
+
+	if (!_tcscmp(command, TEXT("destino"))) {
+		_tprintf(L"[DEBUG] Vou definir o destino %s\n", command);
+	} else if (!_tcscmp(command, TEXT("iniciar"))) {
+		initTrip(plane);
+	} 
+
+	return 1;
+}
+
+void registerPlaneInController(pPlane plane) {
+	//TODO: Comunicar ao controlador que existo
+}
+
+int _tmain(int argc, LPTSTR argv[]) {
 
 #ifdef UNICODE
 	(void)_setmode(_fileno(stdin), _O_WTEXT);
@@ -46,4 +111,14 @@ int _tmain(int argc, TCHAR* argv[]) {
 
 	_tprintf(L"[DEBUG] Cap: %d - Vel: %d\n", plane.maxCapacity, plane.velocity);
 
+	registerPlaneInController(&plane);
+
+	TCHAR command[SIZE];
+	do {
+		_tprintf(L"-> ");
+		_fgetts(command, SIZE, stdin);
+		command[_tcslen(command) - 1] = '\0';
+	} while (processCommand(command, &plane) != 0);
+
+	return 0;
 }
--- TPso2/TPso2/plane.h
+++ TPso2/TPso2/plane.h
@@ -5,8 +5,16 @@
 
 
 typedef struct Aviao Plane, * pPlane;
+typedef struct Coordenada Coordinate, * pCoordinate;
+
+struct Coordenada {
+	int x, y;
+};
+
 struct Aviao {
 	int maxCapacity, velocity;
+	Coordinate current, final;
 };
+
 
 #endif
