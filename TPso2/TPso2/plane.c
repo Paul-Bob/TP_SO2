39fc54ea67faf1d16cbff947c4ea2b916223ea69
Liliana Faustino <lilianafaustino92@gmail.com>
2021-04-18 13:18:53 +0100

    Leitura de comandos a funcionar mais bonito

--- TPso2/TPso2/plane.c
+++ TPso2/TPso2/plane.c
@@ -6,6 +6,28 @@
 
 #include "plane.h"
 
+int getArguments(int argc, TCHAR* argv[], pPlane plane) {
+
+	//TODO: Falta o aeroporto que não faço ideia como vou saber qual o aeroporto onde estou ?????' -.-''' "#$"#$ fuck my life
+
+	if (argc != 3) {
+		_ftprintf(stderr, L"Argumentos para lançar o avião insuficientes\n");
+		return -1;
+	}
+	
+
+	if (sscanf_s(argv[1], "%d", &plane->maxCapacity) == 0) {
+		_ftprintf(stderr, L"Capacidade do avião inválida\n");
+		return -1;
+	}
+	if (sscanf_s(argv[2], "%d", &plane->velocity) == 0) {
+		_ftprintf(stderr, L"Velocidade do avião inválida\n");
+		return -1;
+	}
+
+	return 1;
+}
+
 int _tmain(int argc, TCHAR* argv[]) {
 
 #ifdef UNICODE
@@ -14,28 +36,14 @@ int _tmain(int argc, TCHAR* argv[]) {
 	(void)_setmode(_fileno(stderr), _O_WTEXT);
 #endif
 
-
 	_tprintf(TEXT("Avião a voar #sqn\n"));
 
-	//TODO: Falta o aeroporto que não faço ideia como vou saber qual o aeroporto onde estou ?????' -.-''' "#$"#$ fuck my life
+	Plane plane;
 
-	if (argc != 3) {
-		_ftprintf(stderr, L"Argumentos para lançar o avião insuficientes\n");
-		return -1;
-	}
-	int maxCapacity, velocity; 
-	
-	if (sscanf_s(argv[1], "%d", &maxCapacity) == 0) {
-		_ftprintf(stderr, L"Capacidade do avião inválida\n");
-		return -1;
-	}
-	if (sscanf_s(argv[2], "%d", &velocity) == 0) {
-		_ftprintf(stderr, L"Velocidade do avião inválida\n");
-		return -1;
+	if (getArguments(argc, argv, &plane) == -1) {
+		return 1;
 	}
 
-	_tprintf(L"Cap: %d \nVel: %d\n", maxCapacity, velocity);
-
-
+	_tprintf(L"[DEBUG] Cap: %d - Vel: %d\n", plane.maxCapacity, plane.velocity);
 
 }
--- TPso2/TPso2/plane.h
+++ TPso2/TPso2/plane.h
@@ -1,1 +1,12 @@
-#pragma once
+#ifndef PLANE_H
+#define PLANE_H
+
+#include <windows.h>
+
+
+typedef struct Aviao Plane, * pPlane;
+struct Aviao {
+	int maxCapacity, velocity;
+};
+
+#endif
